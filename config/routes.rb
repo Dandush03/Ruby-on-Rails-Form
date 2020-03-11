317dccf385aecc2ce855f684279cb6725b1d2e35
Dandush03 <d.laloush@outlook.com>
2020-03-11 12:45:14 -0400

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new', to: 'users#new'
-  post 'users/create', to: 'users#create'
+  get 'users', to: 'users#index'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  post 'users' , to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
