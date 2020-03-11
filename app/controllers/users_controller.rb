edbb47857d03de6895f6601547e9bf5a56556d6f
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-03-11 13:20:25 -0300

    added form_for tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_new_path
+      redirect_to new_user_path
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'users/new', to: 'users#new'
-  get 'users/create', to: 'users#create'
-  post 'users/create'
+  post 'users/create', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
