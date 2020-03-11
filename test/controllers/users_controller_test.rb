3e7d0588866fad733d0fd88636e33989da96e3da
Dandush03 <d.laloush@outlook.com>
2020-03-11 18:12:55 -0400

    Fixed Routes Test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_url
     assert_response :success
   end
 
   test "should get create" do
-    get users_create_url
+    get users_url
     assert_response :success
   end
 
